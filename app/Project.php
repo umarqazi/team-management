471a11e1978d059e9f006e945912e267b8e3da59
Shaban Hassan <shaban@gems.techverx.com>
2017-03-27 23:38:39 +0500

    pivot table migration (project_user)

--- app/Project.php
+++ app/Project.php
@@ -14,6 +14,10 @@ class Project extends Model
         return $this->hasMany('App\Hour');
     }
 
+   public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
     // public function accumulative_hours()
     // {
     // 	return $this->hasMany('App\Hour')->selectRaw('sum(productive_hours) as productive_hours');
