053c8200119654e3d8e12f2b5a3e7c818c1d8214
Shaban Hassan <shaban@gems.techverx.com>
2017-04-15 07:43:07 +0500

    Missing Validation Old-keys

--- resources/views/project/create.blade.php
+++ resources/views/project/create.blade.php
@@ -31,7 +31,13 @@
 						</div>
 						<div class="form-group">
 							<label for="technology">Technology:</label>
-							<select id="technology" class="form-control" name="technology[]" multiple></select>
+							<select id="technology" class="form-control" name="technology[]" multiple>
+								@if( !empty(old("technology")) )
+								@foreach(old("technology") as $technology)
+									<option value="{{ $technology }}" {{ "selected" }}>{{ $technology }}</option>
+								@endforeach
+								@endif
+							</select>
 						</div>
 
 						<div class="form-group">
@@ -39,7 +45,7 @@
 							<select class="form-control" id="teamlead" name="teamlead">
 								<option value="">Select Team Lead</option>
 								@foreach($teamleads as $teamlead)
-									<option value="{{$teamlead->id}}">{{$teamlead->name}}</option>
+									<option value="{{$teamlead->id}}" @if(old("teamlead") == $teamlead->id) {{ "selected" }} @endif>{{$teamlead->name}}</option>
 								@endforeach
 							</select>
 						</div>
@@ -49,7 +55,7 @@
 							<select class="form-control" id="developer" name="developer[]" multiple>
 								<option value="">Select Developers</option>
 								@foreach($developers as $developer)
-									<option value="{{$developer->id}}">{{$developer->name}}</option>
+									<option value="{{$developer->id}}" @if( !empty(old("developer")) && in_array($developer->id, old("developer"))){{ "selected" }}@endif>{{$developer->name}}</option>
 								@endforeach
 							</select>
 						</div>
@@ -60,16 +66,16 @@
 						<div class="form-group">
 							<label for="status">Status:</label> <br>
 							<input type="radio" name="status" value="1" class="radio-inline" @if(old("status") == "1") {{ "checked" }} @endif> Active
-							<input type="radio" name="status" value="0" class="radio-inline"> Inactive<br>
+							<input type="radio" name="status" value="0" class="radio-inline" @if(old("status") == "0") {{ "checked" }} @endif> Inactive<br>
 						</div>
 						<br>
 						<div class="form-group">
 							<label for="internal_deadline">Internal Deadline:</label> <br>
-							<input type="datetime-local" class="form-control" name="internal_deadline"><br>
+							<input type="datetime-local" class="form-control" name="internal_deadline" value="{{ old("internal_deadline") }}"><br>
 						</div>
 						<div class="form-group">
 							<label for="external_deadline">External Deadline:</label> <br>
-							<input type="datetime-local" class="form-control" name="external_deadline" ><br>
+							<input type="datetime-local" class="form-control" name="external_deadline" value="{{ old("external_deadline") }}" ><br>
 						</div>
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 						<div class="text-right">
