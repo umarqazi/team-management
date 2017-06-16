af5120095b8ec0b934863127038489e5dc56239a
Shaban Hassan <shaban@gems.techverx.com>
2017-06-16 21:12:58 +0500

    Exception of wordpress staging

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,6 +10,9 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::get('/verge', function(){
+    return File::get(public_path(). '../verge/');
+});
 
 Route::get('/pages', 'PagesController@showPage');
 
