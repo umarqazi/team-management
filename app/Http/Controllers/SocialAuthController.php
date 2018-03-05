6a0073439ba2aa42dfb417f3aa53cb7dced85978
Umar Farooq <umar.farooq@gems.techverx.com>
2018-03-05 16:07:30 +0500

    google Login Restrict.

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -14,7 +14,7 @@ class SocialAuthController extends Controller
 {
     public function redirect($provider)
     {
-        return Socialite::driver($provider)->with(['hd' => 'gems.techverx.com'])->redirect();
+        return Socialite::driver($provider)->with(['hd' => 'gems.techverx.com','gems.com'])->redirect();
     }   
 
     public function callback(SocialAccountService $service, $provider)
